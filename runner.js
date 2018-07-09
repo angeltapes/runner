b32263714672189f2345d4850d7d0b575f9bbfe3
Bobby King <kingrober@gmail.com>
2018-07-09 10:47:49 +0100

    Add files via upload

---
+++ runner.js
@@ -0,0 +1,33 @@
+function setup() {
+  createCanvas(800,400);
+  //createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(0);
+  fill(255);
+  ellipse(10,10,10,10);
+  text("yas",25,25);
+//  drawSprites();
+}
+
+/*var runner;
+
+function preload() {
+
+  //create an animation from a sequence of numbered images
+  //pass the first and the last file name and it will try to find the ones in between
+//  runner = loadAnimation('images/runner1.png', 'images/runner3.png');
+}
+
+function setup() {
+  createCanvas(800, 300);
+}
+
+function draw() {
+  background(0);
+  ellipse(mouseX,mouseY,50,50);
+  //specify the animation instance and its x,y position
+  //animation() will update the animation frame as well
+//  animation(runner, 300, 150);
+}*/
